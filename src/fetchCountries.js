7641cd9b50916965b494ef87b39460a4fc5d4707
ValiaStohnii <valia19961018@gmail.com>
2021-10-29 17:12:03 +0300

    fix fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,21 @@
 import countryCardTpl from './templates/country-card.hbs';
 
+const refs = {
+    countryCards: document.querySelector('.country__cards'),
+    input: document.querySelector('#name-input'),
+}
 
-fetch('https://restcountries.com/v2/name/ukraine')
-    .then(response => { return response.json(); })
-    .then(countries => {
-        console.log(countries);
-        const markup = countries.map(countryCardTpl).join('');
-        console.log(markup);
-    })
-    .catch(error => {console.log(error);});
+fetchCountry()
+    .then(renderCountryCard)
+    .catch(error => { console.log(error) });
+
+function fetchCountry() {
+    return fetch('https://restcountries.com/v2/name/ukraine')
+        .then(response => { return response.json() });
+}
+
+function renderCountryCard(countries) {
+    const markup = countries.map(countryCardTpl).join('');
+    refs.countryCards.innerHTML = markup;
+}
+
