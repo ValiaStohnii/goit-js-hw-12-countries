cfbf970db88394955f10b6c03587c13c6d2794ff
ValiaStohnii <valia19961018@gmail.com>
2021-10-29 18:00:27 +0300

    input

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -5,12 +5,26 @@ const refs = {
     input: document.querySelector('#name-input'),
 }
 
-fetchCountry()
-    .then(renderCountryCard)
-    .catch(error => { console.log(error) });
+refs.input.addEventListener('input', inputChange);
 
-function fetchCountry() {
-    return fetch('https://restcountries.com/v2/name/ukraine')
+// var onKeyupInputDebounced = _.debounce(inputChange, 500);
+// $('input').on('keyup', onKeyupEmailInputDebounced);
+
+function inputChange(e) {
+    e.preventDefault();
+
+    const input = e.currentTarget;
+    console.log(searchInput);
+    const searchInput = input.elements.query.value;
+
+    fetchCountry(`searchInput`)
+        .then(renderCountryCard)
+        .catch(error => console.log(error))
+        .finally(()=>input.reset());
+}
+
+function fetchCountry(country) {
+    return fetch(`https://restcountries.com/v2/name/${country}`)
         .then(response => { return response.json() });
 }
 
@@ -18,4 +32,5 @@ function renderCountryCard(countries) {
     const markup = countries.map(countryCardTpl).join('');
     refs.countryCards.innerHTML = markup;
 }
+
 
