29ca46d3b32788baddbb3872abbb5443753e0c53
ValiaStohnii <valia19961018@gmail.com>
2021-10-30 14:29:03 +0300

    add list

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,27 +1,35 @@
 import countryCardTpl from './templates/country-card.hbs';
+import countriesListTpl from './templates/country-list.hbs';
+import   debounce   from   'lodash.debounce' ;
 
 const refs = {
     countryCards: document.querySelector('.country__cards'),
     input: document.querySelector('#name-input'),
+    countryList:document.querySelector('.countries-list__card'),
 }
 
-refs.input.addEventListener('input', inputChange);
+refs.input.addEventListener('input', debounce(inputChange,500));
 
-// var onKeyupInputDebounced = _.debounce(inputChange, 500);
-// $('input').on('keyup', onKeyupEmailInputDebounced);
 
 function inputChange(e) {
     e.preventDefault();
 
-    const input = e.currentTarget;
+    const searchInput = refs.input.value;
+    // const searchInput = e.currentTarget.elements.query.value;
     console.log(searchInput);
-    const searchInput = input.elements.query.value;
 
-    fetchCountry(`searchInput`)
-        .then(renderCountryCard)
-        .catch(error => console.log(error))
-        .finally(()=>input.reset());
+    if (2 < fetchCountry > 10) {
+        fetchCountry(searchInput)
+            .then(renderCountryList)
+            .catch(error => console.log(error))
+    } else {
+        fetchCountry(searchInput)
+            .then(renderCountryCard)
+            .catch(error => console.log(error))
+    }
+    // .finally(()=>refs.input.reset());
 }
+
 
 function fetchCountry(country) {
     return fetch(`https://restcountries.com/v2/name/${country}`)
@@ -31,6 +39,11 @@ function fetchCountry(country) {
 function renderCountryCard(countries) {
     const markup = countries.map(countryCardTpl).join('');
     refs.countryCards.innerHTML = markup;
+}
+
+function renderCountryList(country) {
+    const markup = country.map(countriesListTpl).join('');
+    refs.countryList.innerHTML = markup;
 }
 
 
